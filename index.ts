d4ced74e38949ddfb58eaa888b6a2a30a2c9f76e
Aaron Traver <travera1@hawkmail.newpaltz.edu>
2021-09-10 13:26:16 -0400

    g

--- index.ts
+++ index.ts
@@ -1,4 +1,5 @@
 import discordJS, { Intents } from 'discord.js'
+import { DefaultMessageNotificationLevels } from 'discord.js/typings/enums'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -10,15 +11,63 @@ const client = new discordJS.Client({
 })
 client.on('ready', () => {
     console.log('the bot is ready now')
-})
 
-client.on('messageCreate',(message) => {
-    if(message.content === 'sing') {
-        message.reply ({
-            content: 'song!'
-        })
+    const guildId = '885638454879326208'
+    const guild = client.guilds.cache.get(guildId)
+    let commands 
+
+    if (guild) {
+        commands = guild.commands 
+    } else {
+        commands = client.application?.commands
     }
 
+    commands?.create({
+        name: 'sing',
+        description: 'replies with song'
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'num1',
+                description: 'the first number.',
+                required: true,
+                type: discordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'the second number.',
+                required: true,
+                type: discordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            }
+        ]
+    })
+})
+
+client.on('interationCreate', async (interaction) => {
+    if(!interaction.isCommand()) {
+        return
+    }
+
+    const {commandName, options } = interaction
+
+    if (commandName === 'sing') {
+        interaction.reply({
+            content: 'song',
+            ephermeral: true,
+        })
+    } else if (commandName === 'add') {
+        const num1 = options.getNumber('num1')!
+        const num2 = options.getNumber('num2')!
+
+        interaction.reply({
+            content: `The sum is ${num1 + num2}`, 
+            ephermeral: true,
+        })
+    }
 })
 
 client.login(process.env.TOKEN)
